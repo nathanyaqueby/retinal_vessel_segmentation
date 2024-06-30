e819e23b681471cf6fd819e13f2410c478d960d9
queby <nathanyaqueby21@gmail.com>
2024-06-30 10:42:40 +0200

    set multipage

--- gui/app.py
+++ gui/app.py
@@ -25,30 +25,35 @@ def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
     )
 
 img = image_select(label = "Select an image",
-                   images = [r"gui/IR_Case_011.png", r"gui/IR_Case_017.png", r"gui/IR_Case_019.png", r"gui/IR_Case_020.png"],
-                   captions = ["IR_Case_011", "IR_Case_017", "IR_Case_019", "IR_Case_020"],
+                   images = [r"gui/train/training_images/IR_Case_011.png", 
+                             r"gui/train/training_images/IR_Case_017.png", 
+                             r"gui/train/training_images/IR_Case_019.png", 
+                             r"gui/train/training_images/IR_Case_020.png",
+                             r"gui/train/training_images/IR_Case_021.png",
+                             r"gui/train/training_images/IR_Case_022.png",],
+                   captions = ["IR_Case_011", "IR_Case_017", "IR_Case_019", "IR_Case_020", "IR_Case_021", "IR_Case_022"],
                    key = "img_select")
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
 
-with col1:
-    draw = ImageDraw.Draw(Image.open(img))
+# with col1:
+draw = ImageDraw.Draw(Image.open(img))
 
-    # Draw an ellipse at each coordinate in points
-    for point in st.session_state["points"]:
-        coords = get_ellipse_coords(point)
-        draw.ellipse(coords, fill="red")
+# Draw an ellipse at each coordinate in points
+for point in st.session_state["points"]:
+    coords = get_ellipse_coords(point)
+    draw.ellipse(coords, fill="red")
 
-    value = streamlit_image_coordinates(img, key="pil")
+value = streamlit_image_coordinates(img, key="pil")
 
-    if value is not None:
-        point = value["x"], value["y"]
+if value is not None:
+    point = value["x"], value["y"]
 
-        if point not in st.session_state["points"]:
-            st.session_state["points"].append(point)
-            st.rerun()
+    if point not in st.session_state["points"]:
+        st.session_state["points"].append(point)
+        st.rerun()
 
-    st.write("Points:", st.session_state["points"])
+st.write("Points:", st.session_state["points"])
 
-with col2:
-    st.image(img)
+# with col2:
+#     st.image(img)
